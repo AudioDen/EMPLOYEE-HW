09d09b7d75ee65ca0e4b8a49b078fd2930c21082
AudioDen <chicagosoundman@gmail.com>
2021-04-22 18:55:06 -0500

    sql start

--- schema.sql
+++ schema.sql
@@ -1,1 +1,28 @@
-ee
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+/* 3 tables*/
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  dep_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10, 2),
+  department_id INT,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY(id)
+);
--- seeds.sql
+++ seeds.sql
@@ -1,1 +1,25 @@
-ee
+INSERT INTO department(dep_name)
+VALUES ("Manager"),
+  ("Sales"),
+  ("Cashier"),
+  ("Stock");
+
+INSERT INTO role(title, salary, department_id)
+VALUES ("Manager", "20000", "1"),
+  ("SalesPerson", "19000", "2"),
+  ("Accounting", "17000", "3"),
+  ("Inventory", "16000", "4");
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ("Adrian", "Newy", "1"),
+  ("Lewis", "Hamilton", "3", "1"),
+  ("Micheal", "Schumi", "4", "1"),
+  ("Jim", "Clark", "2", "1"),
+  ("Alain", "Prost", "2", "1"),
+  ("Jackie", "Stewart", "2", "1"),
+  ("Dan", "Gurney", "2", "1"),
+  ("Nelson", "Piquet", "2", "1"),
+  ("Graham", "Hill", "4", "1"),
+  ("Damon", "Hill", "4", "1"),
+  ("Emerson", "Fittipaldi", "4", "1"),
+  ("Christian", "Fittipaldi", "4", "1");
